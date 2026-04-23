b4d226964404b0af064f58ffc2ee35652abef868
Nrupendra Rout <nrupendrarout65@gmail.com>
2026-04-23 13:23:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello dost kas ho sab!");
+        System.out.println("Hello dost kas ho123456 sab!");
     }
 }
